70738bffa3893169a4a15b8d16d14f7ba65e3abe
dariorod <dario2303@gmail.com>
2023-04-11 14:45:57 -0300

    work in animation

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 
 const Task = ({task, checking}) => {
   
+  const [animation, setAnimation] = useState(false)
+
+  setTimeout(() => {
+    setAnimation(true)
+  }, 5);
 
   return (
-      <div className='item task'>
+      <div className={`task ${animation && "animation"}`}>
         <div>
         <input type="checkbox"
                 className="check" checked={task.check}
